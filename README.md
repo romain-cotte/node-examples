6577d9858c9df7a2d6db2db8caa11e7d312ef7a1
LilMeyer <romain.cotte@gmail.com>
2015-04-28 22:09:21 +0200

    add async examples

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 node-example
 ============
-
+[![Build Status](https://travis-ci.org/LilMeyer/node-example.svg?branch=master)](https://travis-ci.org/LilMeyer/node-example)
 
--- test/async.js
+++ test/async.js
@@ -1,6 +1,9 @@
 var async   = require('async');
 var should  = require('should');
 
+
+
+
 describe('Async', function () {
   it('waterfall', function(done) {
     async.waterfall([
@@ -20,7 +23,59 @@ describe('Async', function () {
     });
   });
 
+  it('parallel', function(done) {
+    async.parallel([
+      function (next) {
+        next(null, 'one');
+      },
+      function (next) {
+        next(null, 'two');
+      }
+    ], function(err, results) { // Optional
+      var arg2 = results.pop();
+      var arg1 = results.pop();
+      arg1.should.eql('one');
+      arg2.should.eql('two');
+      done();
+    });
+  });
 
+  it('series', function(done) {
+    async.series([
+      function (callback) {
+        callback(null, 'one');
+      },
+      function (callback) {
+        callback(null, 'two');
+      }
+    ], function (err, results) {
+      var arg2 = results.pop();
+      var arg1 = results.pop();
+      arg1.should.eql('one');
+      arg2.should.eql('two');
+      done();
+    });
+  });
+
+  it('map', function(done) {
+    var AsyncSquaringLibrary = {
+      squareExponent: 2,
+      square: function(number, callback){
+        var result = Math.pow(number, this.squareExponent);
+        setTimeout(function(){
+          callback(null, result);
+        }, 200);
+      }
+    };
+
+    async.map([1, 2, 3],
+              AsyncSquaringLibrary.square.bind(AsyncSquaringLibrary),
+              function(err, result){
+      should.not.exist(err);
+      result.should.eql([1, 4, 9]);
+      done();
+    });
+  });
 
 
   it('error', function (done) {
