0caa1382cbca183742f49e8baf4c0ad7a8ba3460
LilMeyer <romain.cotte@gmail.com>
2015-06-12 14:12:37 +0200

    add Time test

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 bluebird:
 	mocha test/bluebird.js
 
+time:
+	mocha test/time.js
+
 test:
 	jshint --exclude-path .gitignore .
 	mocha
---
+++ test/time.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var should = require('should'); // jshint ignore:line
+var moment = require('moment');
+
+describe('Time', function () {
+  var date = new Date(2015, 5, 1, 15, 10, 45, 153);
+  it('string', function (done) {
+    /**
+     * Adjust format: see https://docs.adjust.com/en/event-tracking/#reference-server-side-event-tracking
+     */
+    moment(date).format('YYYY-MM-DDThh:mm:ss[Z]ZZ')
+                .should.eql('2015-06-01T03:10:45Z+0200');
+    done();
+  });
+
+});
