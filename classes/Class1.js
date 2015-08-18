4e05c960ac867b9ea603969b4030330f1478af5b
LilMeyer <romain.cotte@gmail.com>
2015-08-18 18:18:53 +0200

    Mongoose: add remove method

---
+++ classes/Class1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Class1 {
+  constructor() {
+    this.property1 = 1;
+  }
+
+  toString() {
+    return 'Property=' + this.property1;
+  }
+}
+
+module.exports = Class1;
--- test/ecmascript6.js
+++ test/ecmascript6.js
@@ -1,12 +1,14 @@
-'use strict';
+// 'use strict';
 
-var should = require('should');// jshint ignore:line
+// var should = require('should');// jshint ignore:line
+// var Class1 = require('../classes/Class1');
 
-describe('Ecmascript 6', function () {
+// describe('Ecmascript 6', function () {
 
-  it('', function () {
+//   it('should test class', function () {
+//     var class1 = new Class1();
+//     console.log(class1.toString());
+//   });
 
-  });
-
-});
+// });
 
