fc50bd4aacee178031176b81478efa5ed3d5f76b
LilMeyer <romain.cotte@gmail.com>
2016-02-02 15:07:21 +0100

    use promises for mongoose examples

--- Makefile
+++ Makefile
@@ -1,28 +1,8 @@
+
 test: redis-flush
-	mocha --harmony
+	mocha --harmony -b
 
 redis-flush:
 	redis-cli flushall
-
-arithmetic:
-	mocha test/arithmetic.js
-
-array:
-	mocha test/array.js
-
-async:
-	mocha test/async.js
-
-bluebird:
-	mocha test/bluebird.js
-
-fs:
-	mocha test/fs.js
-
-redis:
-	mocha test/redis.js
-
-time:
-	mocha test/time.js
 
 .PHONY: bluebird test
--- test/mongoose.js
+++ test/mongoose.js
@@ -1,15 +1,15 @@
-var async    = require('async');
-var mongoose = require('mongoose');
-var ObjectId = mongoose.Types.ObjectId;
-var should   = require('should');
+'use strict';
+const async    = require('async');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const should   = require('should');
 
-var User = require('../models/user');
-mongoose.Promise = require('bluebird');
-
+let User = require('../models/user');
+mongoose.Promise = require('q').Promise
 
 describe('Mongoose', function () {
-  var user;
-  var userContent = {
+  let user;
+  const userContent = {
     firstname: 'Firstname',
     lastname: 'Lastname',
     address: {
@@ -18,165 +18,144 @@ describe('Mongoose', function () {
     }
   };
 
-  before(done => {
-    /**
-     * see http://mongoosejs.com/docs/connections.html
-     */
+  before(() => {
     mongoose.connect('localhost:27017/node-examples');
+  });
+
+  beforeEach(done => {
     user = new User(userContent);
-    (typeof user._id).should.eql('object');
-    async.series([
-      function (next) {
-        User.remove(next);
-      },
-      function (next) {
-        user.save(next);
-      }
-    ], function (err, result) {
-      (typeof result[1][0]._id).should.eql('object');
-      (typeof user._id).should.eql('object');
-      done(err);
-    });
+    User.remove()
+      .then(user.save)
+      .then(u => { done(); })
+      .catch(done);
   });
 
-  after(done => {
-    User.remove({}, function (err, status/*, status no more */) {
-      should.not.exist(err);
-      status.result.ok.should.eql(1);
-      status.result.n.should.eql(2);
-      mongoose.disconnect();
-      done();
-    });
+  after(() => {
+    mongoose.disconnect();
   });
 
-  it('create an ObjectId ', done => {
-    var id = ObjectId.createPk();
-    // createPk return an object
-    (typeof id).should.eql('object');
-    done();
+  it('create an ObjectId', () => {
+    (typeof ObjectId.createPk()).should.eql('object');
   });
 
   it('findOne with a bad ObjectId', done => {
-    User.findOne({ _id: 'badObjectId' }, function (err/*, res*/) {
-      err.message.should.eql('Cast to ObjectId failed for value "badObjectId" at path "_id"');
-      err.name.should.eql('CastError');
-      err.kind.should.eql('ObjectId');
-      err.value.should.eql('badObjectId');
-      err.path.should.eql('_id');
-      done();
-    });
+    User.findOne({ _id: 'badObjectId' })
+      .then(done)
+      .catch(err => {
+        err.message.should.eql('Cast to ObjectId failed for value "badObjectId" at path "_id"');
+        err.name.should.eql('CastError');
+        err.kind.should.eql('ObjectId');
+        err.value.should.eql('badObjectId');
+        err.path.should.eql('_id');
+        done();
+      });
   });
 
   it('findOne with a wrong ObjectId', done => {
-    User.findOne({ _id: ObjectId.createPk() }, function (err, res) {
-      should.not.exist(err);
-      should.not.exist(res);/* res === null */
-      done();
-    });
+    User.findOne({ _id: ObjectId.createPk() })
+      .then(u => {
+        should.not.exist(u);
+        done();
+      })
+      .catch(done);
   });
 
   it('find with a wrong ObjectId', done => {
-    User.find({ _id: ObjectId.createPk() }, function (err, res) {
-      should.not.exist(err);
-      res.should.eql([]);
-      done();
-    });
+    User.find({ _id: ObjectId.createPk() })
+      .then(users => {
+        users.should.eql([]);
+        done();
+      })
+      .catch(done);
   });
 
+
   it('findOne', done => {
-    User.findOne({ _id: user._id }, function (err, res) {
-      should.not.exist(err);
-      res.firstname.should.eql(userContent.firstname);
-      res.lastname.should.eql(userContent.lastname);
-      res.address.city.should.eql(userContent.address.city);
-      res.address.street.should.eql(userContent.address.street);
-      done();
-    });
+    User.findOne({ _id: user._id })
+      .then(u => {
+        u.firstname.should.eql(userContent.firstname);
+        u.lastname.should.eql(userContent.lastname);
+        u.address.city.should.eql(userContent.address.city);
+        u.address.street.should.eql(userContent.address.street);
+        done();
+      });
   });
 
   it('findOne - with a string instead of a ObjectId => it works !', done => {
-    User.findOne({ _id: user._id.toString() }, function (err, res) {
-      should.not.exist(err);
-      res.firstname.should.eql(userContent.firstname);
-      res.lastname.should.eql(userContent.lastname);
-      done();
-    });
+    User.findOne({ _id: user._id.toString() })
+      .then(u => {
+        u.firstname.should.eql(userContent.firstname);
+        u.lastname.should.eql(userContent.lastname);
+        done();
+      })
+      .catch(done)
   });
 
   it('save', done => {
     var user = new User(userContent);
-    user.save(function (err, res, nbSaved) {
-      should.not.exist(err);
-      // console.log(typeof res._id); // object
-      nbSaved.should.eql(1);
-      done();
-    });
+    user.save()
+      .then((u) => {
+        should.exist(u);
+        done();
+      })
+      .catch(done);
   });
 
   it('save a user with a specific _id', done => {
     userContent._id = ObjectId.createPk();
     var user = new User(userContent);
-    user.save(function (err, res/*, nbSaved*/) {
-      should.not.exist(err);
-      res._id.should.eql(userContent._id);
-      done();
-    });
+    user.save()
+      .then(u => {
+        u._id.should.eql(userContent._id);
+        done()
+      })
+      .catch(done);
   });
 
   it('update', done => {
     User.update({ _id: user._id },
-                { firstname: 'newFirstname' },
-                function (err, status /*, no more status here */ ) {
-      should.not.exist(err);
-      status.n.should.eql(1);
-      status.ok.should.eql(1);
-      done();
-    });
+                { firstname: 'newFirstname' })
+      .then(r => {
+        r.ok.should.eql(1);
+        r.n.should.eql(1);
+        done();
+      })
   });
 
   it('count', done => {
-    User.count({}, function (err, count, param /* undefined */) {
-      should.not.exist(err);
-      count.should.eql(3);
-      should.not.exist(param);
-      done();
-    });
+    User.count({})
+      .then(c => {
+        c.should.eql(1);
+        done();
+      })
+      .catch(done);
   });
 
   it('stream', done => {
     var stream = User.find().stream();
     var count = 0;
-    stream.on('data', function (/*doc*/) {
+    stream.on('data', () => {
       count++;
     });
 
-    stream.on('error', function (err) {
+    stream.on('error', err => {
       should.not.exist(err);
     });
 
-    stream.on('close', function () {
-      count.should.eql(3);
+    stream.on('close', () => {
+      count.should.eql(1);
       done();
     });
   });
 
   it('remove', done => {
-    user.remove(function (err, res) {
-      should.not.exist(err);
-      res.firstname.should.eql(user.firstname);
-      res.lastname.should.eql(user.lastname);
-      res._id.should.eql(user._id);
-      done();
-    });
-  });
-
-  it('with promises', (done) => {
-    User.find()
-      .then((users) => {
-        should.exist(users);
+    user.remove()
+      .then(r => {
+        r.firstname.should.eql(user.firstname);
+        r.lastname.should.eql(user.lastname);
+        r._id.should.eql(user._id);
         done();
       })
-      .catch(done);
   });
 
 });
