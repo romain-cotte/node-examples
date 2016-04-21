ab891a9654866a721d08ee54154aec76f8db507f
LilMeyer <romain.cotte@gmail.com>
2016-04-21 18:18:17 +0200

    Add sinon examples

--- test/sinon.js
+++ test/sinon.js
@@ -3,27 +3,39 @@
 const sinon = require('sinon')
 const should = require('should')
 
-const f = {
-  incr: i => i + 1
-}
-
 describe('Sinon', () => {
 
-  it('f', () => {
-    f.incr(1).should.eql(2)
-  })
+  describe('Sync function', () => {
+    const f = {
+      incr: i => i + 1
+    }
 
-  it('stub the function', () => {
-    let stub = sinon.stub(f, 'incr', i => i + 3)
-    f.incr(1).should.eql(4)
-    stub.restore() // could be f.incr.restore()
-  })
+    it('f', () => {
+      f.incr(1).should.eql(2)
+    })
 
-  it('mock the function', () => {
-    let mock = sinon.mock(f)
-    let expectation = mock.expects('incr');
-    f.incr(1);
-    expectation.calledOnce.should.be.true()
+    it('stub the function', () => {
+      const stub = sinon.stub(f, 'incr', i => i + 3)
+      f.incr(1).should.eql(4)
+      sinon.assert.calledOnce(stub);
+      stub.restore() // could be f.incr.restore()
+    })
+
+    it('mock the function', () => {
+      const mock = sinon.mock(f)
+      const expectation = mock.expects('incr');
+      should.not.exist(f.incr(1))
+      expectation.once()
+      expectation.verify()
+      mock.restore()
+    })
+
+    it('spy the function', () => {
+      const spy = sinon.spy(f, 'incr')
+      f.incr(10).should.eql(11)
+      spy.withArgs(10).calledOnce.should.be.true()
+      spy.restore()
+    })
   })
 
 })
