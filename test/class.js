447a72c367370f9335ec17cf50d8ab1d6c9f767a
LilMeyer <romain.cotte@gmail.com>
2016-05-16 12:59:08 +0200

    add use strict statement

--- test/class.js
+++ test/class.js
@@ -1,7 +1,8 @@
+'use strict'
+
 const A = require('../classes/A.js')
 const B = require('../classes/B.js')
 const C = require('../classes/C.js')
-'use strict'
 
 const staticA = require('../classes/StaticA.js')
 const should = require('should') //eslint-disable-line
