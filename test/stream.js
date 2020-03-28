0b143b5cd80d2966b7647b772a10262cc35a03d7
Romain Cotte <romain.cotte@gmail.com>
2020-03-28 21:00:30 +0100

    add stream example

---
+++ test/stream.js
@@ -0,0 +1,32 @@
+const should = require('should');
+const Stream = require('stream');
+
+describe('Stream', () => {
+  let cnt = 0;
+  const readable = new Stream.Readable({
+    objectMode: true,
+    async read() {
+      if (++cnt <= 5) {
+        this.push(cnt);
+      } else {
+        this.push(null);
+      }
+    }
+  })
+
+  it('should write from a readable stream', done => {
+    let results = [];
+    let writable = new Stream.Writable({
+      objectMode: true,
+      write(chunk, enc, next) {
+        results.push(chunk)
+        next();
+      }
+    });
+    Stream.pipeline(readable, writable, (err) => {
+      should.not.exist(err);
+      results.should.eql([ 1, 2, 3, 4, 5])
+      done();
+    })
+  });
+});
