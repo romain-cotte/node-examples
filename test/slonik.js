7878900a78c62a0da8782b8aff7b92917bc13d83
Romain Cotte <romain.cotte@gmail.com>
2021-01-26 15:39:03 +0100

    add slonik

---
+++ test/slonik.js
@@ -0,0 +1,16 @@
+const { sql } = require('slonik');
+const should = require('should'); // eslint-disable-line
+
+describe('slonik', () => {
+  it('method name', async () => {
+    const myVar1 = 'a';
+    const myVar2 = 'b';
+    const query = sql`SELECT ${myVar2}`;
+    const mainQuery = sql`SELECT *, ${myVar1} FROM (${query})`
+    mainQuery.should.eql({
+      sql: 'SELECT *, $1 FROM (SELECT $2)',
+      type: 'SLONIK_TOKEN_SQL',
+      values: [ 'a', 'b' ]
+    });
+  });
+});
