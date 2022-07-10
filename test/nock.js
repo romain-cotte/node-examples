9e6d52897c9468c9c9fd105130b7de9f6caa3ce3
Romain Cotte <romain.cotte@gmail.com>
2022-07-10 23:23:40 +0200

    feat(packages): remove request-promises

--- test/nock.js
+++ test/nock.js
@@ -1,36 +1,70 @@
 import nock from 'nock';
-import rp from 'request-promise';
 import should from 'should'; // eslint-disable-line
+import https from 'https';
+
 
 describe('Nock', () => {
 
-  let url = 'http://test.com';
+  let url = 'https://test.com';
   before(() => {
     nock(url)
       .post('/users')
-      .reply(201, { id: '123456' });
+      .reply(200, { id: '123456' });
   });
 
   it('post test', done => {
+    const data = JSON.stringify({
+      username: 'username',
+      email: 'test@test.com'
+    });
     const options = {
-      url: `${url}/users`,
+      hostname: 'test.com',
+      port: 443,
+      path: '/users',
       method: 'POST',
-      headers: { authorization: 'Bearer token' },
-      body: {
-        username: 'username',
-        email: 'test@test.com'
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
       },
-      json: true,
-      resolveWithFullResponse: true
     };
 
-    rp(options)
-      .then(({ statusCode, body }) => {
-        statusCode.should.eql(201);
-        body.should.eql({ id: '123456' });
+    const req = https.request(options, res => {
+      res.statusCode.should.eql(200);
+
+      res.on('data', d => {
+        d.toString().should.eql('{"id":"123456"}');
         done();
-      })
-      .catch(done);
+      });
+    });
+
+    req.on('error', error => {
+      console.error(error);
+    });
+
+    req.write(data);
+    req.end();
+
+
+
+    // const options = {
+    //   url: `${url}/users`,
+    //   method: 'POST',
+    //   headers: { authorization: 'Bearer token' },
+    //   body: {
+    //     username: 'username',
+    //     email: 'test@test.com'
+    //   },
+    //   json: true,
+    //   resolveWithFullResponse: true
+    // };
+
+    // rp(options)
+    //   .then(({ statusCode, body }) => {
+    //     statusCode.should.eql(201);
+    //     body.should.eql({ id: '123456' });
+    //     done();
+    //   })
+    //   .catch(done);
   });
 
 });
